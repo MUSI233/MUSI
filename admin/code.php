0559fb833a70d48888cb1fff3de0e7182e1d45dd
MUSI <80134141+MUSI233@users.noreply.github.com>
2021-08-05 10:15:40 +0800

    Delete admin directory

--- admin/code.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-define('ROOT_PATH', dirname(__FILE__));
-require '../includes/ValidateCode.php';
-$_vc = new ValidateCode();
-$_vc->doimg();
-$_SESSION['vc_code'] = $_vc->getCode();
-?>
